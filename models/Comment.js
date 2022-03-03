bd2c7ea8d1ac212dc95667cab7fecda5f58a4c17
rrich-kray <rrich.kray@gmail.com>
2022-03-03 12:10:23 -0500

    added comment model

---
+++ models/Comment.js
@@ -0,0 +1,17 @@
+const sequelize = require("../config/connection"); // not yet written
+const { Model, DataTypes } = require("sequelize");
+
+class Comment extends Model {}
+
+Comment.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  comment_text: {
+    type: DataType.STRING,
+    allowNull: false,
+  },
+});
