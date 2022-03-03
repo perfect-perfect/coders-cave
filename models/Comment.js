ac6d47d8476185b6786ce41888a847b5733ca811
rrich-kray <rrich.kray@gmail.com>
2022-03-03 12:25:37 -0500

    added to comment model

--- models/Comment.js
+++ models/Comment.js
@@ -3,29 +3,37 @@ const { Model, DataTypes } = require("sequelize");
 
 class Comment extends Model {}
 
-Comment.init({
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  comment_text: {
-    type: DataType.STRING,
-    allowNull: false,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "user",
-      id: "id",
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    comment_text: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        id: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        id: "id",
+      },
     },
   },
-  post_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "post",
-      id: "id",
-    },
-  },
-});
+  {
+    sequelize,
+    timeStamps: false,
+    underscored: true,
+    name: "comment",
+  }
+);
