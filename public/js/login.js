ac53ecc16368e32256a1815a735de8d261e8c482
rrich-kray <rrich.kray@gmail.com>
2022-03-03 19:47:16 -0500

    completed login and signup JS functions

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
-const loginHandler = async (event) => {
+const login = async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector("").value; // will insert element when html is ready
+  const email = document.querySelector("").value;
   const password = document.querySelector("").value;
 
   if (email & password) {
@@ -24,4 +24,30 @@ const loginHandler = async (event) => {
   }
 };
 
-const signup = async () => {};
+const signup = async (event) => {
+  event.preventDefault();
+
+  const userName = dcoument.querySelector("").value;
+  const email = document.querySelector("").value;
+  const password = document.querySelector("").value;
+
+  if (userName & email & password) {
+    const response = await fetch("api/users/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        userName,
+        email,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
