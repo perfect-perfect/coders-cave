0b3d9249b04ca2c1b2752e26874e37c60e20504b
rrich-kray <rrich.kray@gmail.com>
2022-03-05 12:56:28 -0500

    started on findall post route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,7 +1,26 @@
-const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const { Post, User, Comment, Vote } = require('../../models');
+const router = require("express").Router();
+const { cp } = require("fs");
+const { userInfo } = require("os");
+const sequelize = require("../../config/connection");
+const { Post, User, Comment, Vote } = require("../../models");
 
 // post route go here
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["title", "post_url", "user_id"],
+    order: [["created_at", "DESC"]],
+    include: {
+      model: Comment,
+      attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {},
+      ],
+    },
+  }).then((postData) => {});
+});
 
-module.exports = router; 
+module.exports = router;
