059a05911d85c4cb84f1d1c10f0e1a4d2bf7e7c7
rrich-kray <rrich.kray@gmail.com>
2022-03-10 15:24:48 -0500

    started on chat feature

--- controllers/chat-routes.js
+++ controllers/chat-routes.js
@@ -1,3 +1,12 @@
 const router = require("express").Router();
+const { Chat } = require("../models/Chat-Comments");
+const withAuth = require("../utils/auth");
 
-router.get("/");
+router.get("/", (req, res) => {
+  Chat.findAll({
+    attributes: ["username", "content"],
+  }).then((chatData) => {
+    const chatPosts = chatData.map((post) => post.get({ plain: true }));
+    res.render("chat", posts);
+  });
+});
