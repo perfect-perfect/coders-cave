9d3cb8545fae79c91616cffd98851eebc319f3da
rrich-kray <rrich.kray@gmail.com>
2022-03-12 13:55:26 -0500

    added chat

--- server.js
+++ server.js
@@ -2,7 +2,26 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const io = require("socket.io")(3000);
 
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
